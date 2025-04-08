d50622afb000fc9caf041c05b4b786903584dddb
anannicole <158208763+anannicole@users.noreply.github.com>
2025-04-08 10:13:34 -0600

    primercom

---
+++ sumativa/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'sumativa',
+  webDir: 'www'
+};
+
+export default config;
